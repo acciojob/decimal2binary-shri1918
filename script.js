085cf3f57bcbaf00cb8ff2d973b9237cb37cd8fa
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-07-15 20:11:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function decimalToBinary(num) {
   // Write your code here
+	if (decimal === 0) {
+    return '0';
+  }
+
+  let binary = '';
+
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
